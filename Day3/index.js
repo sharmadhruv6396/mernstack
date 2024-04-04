8e732fa218e5efe20306577b993512525b332f07
___sharmadhruv <sharmadhruv2509@gmail.com>
2024-04-04 16:34:58 +0530

    Javascrpit and Grid

---
+++ Day3/index.js
@@ -0,0 +1,190 @@
+// console.log("Hello");
+
+
+// var message="Hello World";
+// var age=10;
+// age="Done";
+
+// console.log(message);
+
+//loosely typed
+
+//strictly types
+
+//statically typed
+//dynamically type
+// function env(){
+
+// comsole.log(message);
+
+// var message="hello";
+
+// console.log(message);
+
+// message="else"; 
+
+// console.log(message);
+// }
+// env();
+// function env(){
+//     for(var i=0;i<5;i++){
+//         console.log(' ',i);
+//     }
+//     console.log(i);
+// }
+
+// env();
+
+
+// const message="Hello";
+// console.log(message);
+
+// let age='abbd';
+// age='abbd';
+// console.log(typeof(age))
+
+// let arr=['a','b','c','d'];
+// arr[0]='abcd';
+// console.log(arr);
+
+// let str='12'
+
+// let age=20;
+
+// let ams='12'+20;
+
+// console.log(ans);
+
+
+// let str='12'
+
+// let p=parseInt(str);
+
+// let n=Number(str);
+
+// console.log(p,n)
+
+
+// let a='12';
+
+// let b=13;
+
+// if(a==b){
+//     console.log('yes');
+// }
+// else{
+//     console.log('no')
+// }
+
+// if(a==b){
+//     console.log('yes again');
+// }
+// else{
+//     console.log('no again')
+// }
+
+
+// let a=['a','b','c','d'];
+// let cpy=[...a];
+
+// console.log(a,cpy);
+
+// if(a==cpy){
+//     console.log('1.yes');
+// }
+// else{
+//     console.log('1.no');
+// }
+
+
+
+// const a="My";
+// const b="Name";
+// const c="is";
+// const d=':';
+// const e='Dhruv';
+// const ans=`$(a) $(b) $(c)$(d) $(str)`;
+// const ans2=a+''+b+''+c+''+d+'';
+// console.log(ans);
+// console.log(ans2);
+
+// function print(str){
+//     console.log(str);
+// }
+// print('Hello');
+
+
+
+// function sum(a,b){
+//     if(a && b ){
+
+//     console.log(a+b);
+// }
+// else{
+//     console.log(a);
+// }
+// }
+// sum(10);
+
+
+
+// //function declaration
+// function print(){
+//     console.log('--');
+// }
+// //funtion assignment (named)
+// const a=function print(){
+//     console.log('--');
+// }
+// //function assignment(annymous)
+// const b=function print(){
+//     console.log('--');
+// }
+
+// //arrow function assignment
+// let c=()=>{
+//     console.log(--);
+// }
+// print();
+// a();
+// b();
+// c();
+
+
+// let a=22;
+
+// switch(a){
+//     case 1:
+//         console.log("one");
+//         break;
+//         case 2:
+//             console.log("two");
+//             break;
+//         default:
+//             console.log('not valid');
+// }
+
+
+// const obj=new Object();
+// obj[12]='twelve';
+// console.log(obj);
+
+// const obj= {
+//     1:'one',
+//     'name': 'Dhruv',
+//     12: 'twelve',
+// };
+// console.log(obj);
+
+const obj= {
+    1:'one',
+    'name': 'Dhruv',
+    12: 'twelve',
+};
+console.log(obj);
+
+const x=prompt('pleaase enter the property');
+console.log(x);
+
+
+
